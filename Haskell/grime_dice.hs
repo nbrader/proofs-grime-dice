3b6d5bffd188d91f19a4df42361b09dbbedd3d61
nbrader <nbrader+github@gmail.com>
2024-11-30 12:31:44 +0000

    Working out joint probabilities in Haskell.

--- Haskell/grime_dice.hs
+++ Haskell/grime_dice.hs
@@ -1,153 +1,83 @@
 #!/usr/bin/env stack
--- stack --resolver lts-20.5 ghci --package QuickCheck
+-- stack --resolver lts-20.5 ghci --package QuickCheck --package containers-0.6.5.1
 import Test.QuickCheck
 import Data.List (nub)
+import qualified Data.Map as Map
+import Data.Map (Map)
 import Control.Monad
+import Data.Ratio ((%))
 
--- Define data types for both magmas
-data M1 = A | B | C | D deriving (Eq, Show, Enum, Bounded)
-data M2 = P | Q | R | S deriving (Eq, Show, Enum, Bounded)
+type Prob s = s
+type Val v = v
 
--- First magma operation (non-associative)
-magmaOp1 :: M1 -> M1 -> M1
-magmaOp1 A A = B
-magmaOp1 A B = A
-magmaOp1 A C = C
-magmaOp1 A D = D
-magmaOp1 B A = A
-magmaOp1 B B = C
-magmaOp1 B C = D
-magmaOp1 B D = B
-magmaOp1 C A = C
-magmaOp1 C B = D
-magmaOp1 C C = B
-magmaOp1 C D = A
-magmaOp1 D A = D
-magmaOp1 D B = C
-magmaOp1 D C = A
-magmaOp1 D D = B
+data Die v s = Die {diePMF :: Map [v] s} deriving (Eq, Show)
 
--- Second magma operation (associative - Klein four-group)
-magmaOp2 :: M2 -> M2 -> M2
-magmaOp2 P P = P
-magmaOp2 P Q = Q
-magmaOp2 P R = R
-magmaOp2 P S = S
-magmaOp2 Q P = Q
-magmaOp2 Q Q = P
-magmaOp2 Q R = S
-magmaOp2 Q S = R
-magmaOp2 R P = R
-magmaOp2 R Q = S
-magmaOp2 R R = P
-magmaOp2 R S = Q
-magmaOp2 S P = S
-magmaOp2 S Q = R
-magmaOp2 S R = Q
-magmaOp2 S S = P
+-- Example dice definitions using `Map`
+redDie :: Die Rational Rational
+redDie = Die $ Map.fromList [([4], 5 % 6), ([9], 1 % 6)]
 
--- Generic functions that work with either magma
-class (Eq a, Show a) => MagmaElement a where
-    carrier :: [a]
-    op :: a -> a -> a
+greenDie :: Die Rational Rational
+greenDie = Die $ Map.fromList [([0], 1 % 6), ([5], 5 % 6)]
 
-instance MagmaElement M1 where
-    carrier = [A .. D]
-    op = magmaOp1
+blueDie :: Die Rational Rational
+blueDie = Die $ Map.fromList [([2], 3 % 6), ([7], 3 % 6)]
 
-instance MagmaElement M2 where
-    carrier = [P .. S]
-    op = magmaOp2
+purpleDie :: Die Rational Rational
+purpleDie = Die $ Map.fromList [([1], 2 % 6), ([6], 4 % 6)]
 
--- Check if middle associativity holds for a specific middle element
-isMiddleAssoc :: MagmaElement a => a -> Bool
-isMiddleAssoc m = and [op (op a m) b == op a (op m b) | 
-                      a <- carrier, b <- carrier]
+yellowDie :: Die Rational Rational
+yellowDie = Die $ Map.fromList [([3], 4 % 6), ([8], 2 % 6)]
 
--- Helper function for folding with the magma operation
-foldMagma :: MagmaElement a => a -> [a] -> a
-foldMagma = foldl op
+-- Example enumeration of dice colors
+data DieColours = Red | Green | Blue | Purple | Yellow deriving (Eq, Show, Enum, Bounded)
 
--- Check if fold-left-combine middle associativity holds
-isFoldLeftCombineMiddleAssoc :: MagmaElement a => a -> Bool
-isFoldLeftCombineMiddleAssoc m = and [
-    let result1 = foldMagma x xs
-        result2 = foldMagma y ys
-        combined1 = op result1 result2
-        combined2 = foldMagma x (xs ++ [m] ++ ys)
-    in combined1 == combined2 |
-    x <- carrier,
-    y <- carrier,
-    xs <- sequence [carrier],
-    ys <- sequence [carrier]
-    ]
+allDieColours :: [DieColours]
+allDieColours = [minBound .. maxBound]
 
--- Check if an element is a generator
-isGeneratorM1 :: M1 -> Bool
-isGeneratorM1 m = length (generateElements m) == length (carrier :: [M1])
-  where
-    generateElements x = nub $ concat $ take 4 $ iterate genStep [x]
-    genStep elems = [op a b | a <- elems, b <- carrier]
+-- Enumeration for outcomes of dice comparison
+data Outcome = FirstGreater | SecondGreater | Tie deriving (Eq, Show, Ord)
 
-isGeneratorM2 :: M2 -> Bool
-isGeneratorM2 m = length (generateElements m) == length (carrier :: [M2])
-  where
-    generateElements x = nub $ concat $ take 4 $ iterate genStep [x]
-    genStep elems = [op a b | a <- elems, b <- carrier]
+-- Function to verify probabilities sum to 1
+validateDie :: (Eq s, Num s) => Die v s -> Bool
+validateDie die = sum (Map.elems $ diePMF die) == 1
 
--- Properties for both magmas
-class (MagmaElement a, Arbitrary a) => TestMagma a where
-    testMagma :: String -> a -> IO ()
+-- Function to calculate joint probability of two dice
+jointProbability :: (Ord v, Num s) => Die v s -> Die v s -> Die v s
+jointProbability (Die pmf1) (Die pmf2) =
+    Die $ Map.fromListWith (+)
+        [ (v1 ++ v2, p1 * p2) | 
+            (v1, p1) <- Map.toList pmf1,
+            (v2, p2) <- Map.toList pmf2
+        ]
 
-instance TestMagma M1 where
-    testMagma name _ = testMagmaGenericM1 name (carrier :: [M1])
+-- Function to calculate PMF over (first > second, first < second, tie)
+pmfOutcomeComparison :: (Ord v, Num s) => Die v s -> Die v s -> Map Outcome s
+pmfOutcomeComparison (Die pmf1) (Die pmf2) =
+    Map.fromListWith (+)
+        [ (outcome, p1 * p2)
+        | (v1, p1) <- Map.toList pmf1,
+          (v2, p2) <- Map.toList pmf2,
+          let outcome = case compare (head v1) (head v2) of
+                            GT -> FirstGreater
+                            LT -> SecondGreater
+                            EQ -> Tie
+        ]
 
-instance TestMagma M2 where
-    testMagma name _ = testMagmaGenericM2 name (carrier :: [M2])
+-- Test PMF over outcome comparisons for all pairs of dice
+testAllPairsOutcomeComparison :: [(DieColours, Die Rational Rational)] -> IO ()
+testAllPairsOutcomeComparison dice = do
+    forM_ [(c1, d1, c2, d2) | (c1, d1) <- dice, (c2, d2) <- dice] $ \(c1, d1, c2, d2) -> do
+        let pmfComparison = pmfOutcomeComparison d1 d2
+        putStrLn $ "PMF over outcomes (" ++ show c1 ++ " vs " ++ show c2 ++ "):"
+        print pmfComparison
 
-testMagmaGenericM1 :: String -> [M1] -> IO ()
-testMagmaGenericM1 name elems = do
-    putStrLn $ "\nTesting " ++ name ++ ":"
-    putStrLn "Testing which elements are generators:"
-    mapM_ (\m -> putStrLn $ show m ++ " is " ++ 
-           (if isGeneratorM1 m then "" else "not ") ++ "a generator") elems
-    
-    putStrLn "\nTesting which elements satisfy middle associativity:"
-    mapM_ (\m -> putStrLn $ show m ++ " does " ++ 
-           (if isMiddleAssoc m then "" else "not ") ++ 
-           "satisfy middle associativity") elems
-    
-    putStrLn "\nTesting which elements satisfy fold-left-combine middle associativity:"
-    mapM_ (\m -> putStrLn $ show m ++ " does " ++ 
-           (if isFoldLeftCombineMiddleAssoc m then "" else "not ") ++ 
-           "satisfy fold-left-combine middle associativity") elems
-
-testMagmaGenericM2 :: String -> [M2] -> IO ()
-testMagmaGenericM2 name elems = do
-    putStrLn $ "\nTesting " ++ name ++ ":"
-    putStrLn "Testing which elements are generators:"
-    mapM_ (\m -> putStrLn $ show m ++ " is " ++ 
-           (if isGeneratorM2 m then "" else "not ") ++ "a generator") elems
-    
-    putStrLn "\nTesting which elements satisfy middle associativity:"
-    mapM_ (\m -> putStrLn $ show m ++ " does " ++ 
-           (if isMiddleAssoc m then "" else "not ") ++ 
-           "satisfy middle associativity") elems
-    
-    putStrLn "\nTesting which elements satisfy fold-left-combine middle associativity:"
-    mapM_ (\m -> putStrLn $ show m ++ " does " ++ 
-           (if isFoldLeftCombineMiddleAssoc m then "" else "not ") ++ 
-           "satisfy fold-left-combine middle associativity") elems
-
--- Arbitrary instances
-instance Arbitrary M1 where
-    arbitrary = elements [A .. D]
-
-instance Arbitrary M2 where
-    arbitrary = elements [P .. S]
-
--- Main function to test both magmas
+-- Main function to test validation and joint probabilities
 main :: IO ()
 main = do
-    testMagma "First Magma (Non-associative)" (undefined :: M1)
-    testMagma "Second Magma (Klein four-group)" (undefined :: M2)
+    let dice = zip allDieColours [redDie, greenDie, blueDie, purpleDie, yellowDie]
+    putStrLn "Validating dice..."
+    forM_ dice $ \(color, die) -> do
+        let isValid = validateDie die
+        putStrLn $ show color ++ ": " ++ if isValid then "Valid" else "Invalid"
+    putStrLn "\nTesting outcome comparison for all pairs:"
+    testAllPairsOutcomeComparison dice
